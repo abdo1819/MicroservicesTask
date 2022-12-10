47d7ac471ee94f6de9ae6d47333e7704c34484f3
abdelrahman ragab <abdo14hashem15@gmail.com>
2022-12-10 13:20:22 +0200

    add unit testing

--- src/Services/CartService/Cart.UnitTest/UnitTest1.cs
+++ src/Services/CartService/Cart.UnitTest/UnitTest1.cs
@@ -6,23 +6,55 @@ using Cart.API.Repastory;
 using Confluent.Kafka;
 using Moq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace Cart.UnitTest
 {
     [TestClass]
     public class CartControllerTest
     {
-        private static Mock<ICartRepastory>? _mockCartRepastory;
-        private static Mock<IKafkaDependentProducer<Null, string>>? _mockKafkaProducer;
+        private readonly Mock<ICartRepository>? _mockCartRepastory;
+        private readonly Mock<IKafkaDependentProducer<Null, string>>? _mockKafkaProducer;
+        private readonly Mock<HttpContext> _contextMock;
+
+        public CartControllerTest()
+        {
+        _mockCartRepastory = new Mock<ICartRepository>();
+        _mockKafkaProducer = new Mock<IKafkaDependentProducer<Null, string>>();
+        _contextMock = new Mock<HttpContext>();
+        }
 
         [ClassInitialize]
         public static void init(TestContext context)
         {
-            _mockCartRepastory = new Mock<ICartRepastory>();
-            _mockKafkaProducer = new Mock<IKafkaDependentProducer<Null, string>>();
+            
             
         }
+        [TestMethod]
+        public async Task GetCustomerCart_CreateNewCartInotexist_OKCart()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            _mockCartRepastory!.Setup(x => x.GetCart(customerId)).ReturnsAsync((CartModel?)null);
+            _mockCartRepastory.Setup(x => x.CreateCart(customerId)).ReturnsAsync(new CartModel() { CustomerId = customerId });
+            
+            var controller = new CartController(_mockKafkaProducer!.Object, _mockCartRepastory.Object);
+            controller.ControllerContext.HttpContext = _contextMock.Object;
+            // Act
+            var actionResult = await controller.Get(customerId) ;
 
+            // Assert            
+            var result = actionResult.Result as OkObjectResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.StatusCode, 200);
+            Assert.IsInstanceOfType(result.Value, typeof(CartModel));
+            var cart = result.Value as CartModel;
+            Assert.IsNotNull(cart);
+            Assert.AreEqual(cart.CustomerId, customerId);
+            
+            _mockCartRepastory.Verify(x => x.GetCart(customerId), Times.Once);
+            _mockCartRepastory.Verify(x => x.CreateCart(customerId), Times.Once);            
+        }
         [TestMethod]
         public void PostAddToCart_newCartAndValidItem_createCartAddItemOK()
         {
@@ -34,8 +66,8 @@ namespace Cart.UnitTest
                 Quantity = 1
             };
             _mockCartRepastory!.Setup(x => x.GetCart(customerId)).ReturnsAsync((CartModel?)null);
-            _mockCartRepastory.Setup(x => x.CreateCart(customerId)).ReturnsAsync(new CartModel());
-            _mockCartRepastory.Setup(x => x.AddToCart(customerId, item)).ReturnsAsync(true);
+            _mockCartRepastory.Setup(x => x.CreateCart(customerId)).ReturnsAsync(new CartModel() { CustomerId = customerId });
+            _mockCartRepastory.Setup(x => x.SetItemInCart(customerId, item)).ReturnsAsync(true);
             
             _mockKafkaProducer!.Setup(x => x.ProduceAsync("cart", It.IsAny<AddToCartMessage>())).ReturnsAsync(new DeliveryResult<Null, string>());
             
@@ -48,7 +80,7 @@ namespace Cart.UnitTest
             Assert.IsInstanceOfType(result, typeof(OkResult));
 
             _mockCartRepastory.Verify(x => x.CreateCart(customerId), Times.Once);
-            _mockCartRepastory.Verify(x => x.AddToCart(customerId, item), Times.Once);
+            _mockCartRepastory.Verify(x => x.SetItemInCart(customerId, item), Times.Once);
             _mockKafkaProducer.Verify(x => x.ProduceAsync("cart", It.IsAny<AddToCartMessage>()), Times.Once);
             
         }
